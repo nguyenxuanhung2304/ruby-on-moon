ff80c08cca5d1dd7db0c1778fe65af3b093a2806
Nguyen Xuan Hung <mongtamquoc2015@gmail.com>
2023-10-31 13:42:08 +0700

    add: rubocop gem and yardoc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
+# The `User` class represents a user with a name and age.
+#
+# This class is used to create user objects with a name and age.
+#
+# @example
+#   user = User.new("John", 30)
+#   puts user.name
+#   # => "John"
+#   puts user.age
+#   # => 30
 class User
-  attr_reader :name, :age
+  # @!attribute [r] name
+  # @return [String] The name of the user.
+  attr_reader :name
 
+  # @!attribute [r] age
+  # @return [Integer] The age of the user.
+  attr_reader :age
+
+  # Initializes a new User with the specified name and age.
+  #
+  # @param name [String] The name of the user.
+  # @param age [Integer] The age of the user.
   def initialize(name, age)
     @name = name
     @age = age
--- middleware/logging.rb
+++ middleware/logging.rb
@@ -1,15 +1,35 @@
-require 'logger'
-
+# The `Logging` class is responsible for logging information about HTTP requests and responses.
+#
+# This middleware class logs request and response details to a log file, including information
+# about the route, controller, and view being processed.
 class Logging
-  attr_reader :logger, :path, :env
+  # @!attribute [r] logger
+  #   @return [Logger] The logger instance used for logging.
+  attr_reader :logger
 
+  # @!attribute [r] path
+  #   @return [String] The path of the current request.
+  attr_reader :path
+
+  # @!attribute [r] env
+  #   @return [Hash] The environment hash containing information about the HTTP request.
+  attr_reader :env
+
+  # Includes the Parser module for route parsing functionality.
   include Parser
 
+  # Initializes a new instance of Logging.
+  #
+  # @param app [Object] The application or middleware to which this logger is attached.
   def initialize(app)
     @app = app
     @logger = make_logger
   end
 
+  # Logs information about the HTTP request and response.
+  #
+  # @param env [Hash] The environment hash containing information about the HTTP request.
+  # @return [Array] The HTTP response.
   def call(env)
     @path = env['REQUEST_PATH']
     @env = env
@@ -19,6 +39,9 @@ class Logging
 
   private
 
+  # Creates a logger instance for logging.
+  #
+  # @return [Logger] A Logger instance for logging.
   def make_logger
     file_path = 'log/development.log'
     unless File.exist?(file_path)
@@ -28,6 +51,7 @@ class Logging
     Logger.new(file_path)
   end
 
+  # Logs information about the request and response.
   def make_log
     instance_klass, view_template, action = resource_controller
     request_method = env['REQUEST_METHOD']
